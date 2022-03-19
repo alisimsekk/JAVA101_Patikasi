4524d2a7a95a7669a3fd17ca5a9d7b2921331ac2
alisimsek <ali.simsek035@gmail.com>
2022-03-19 13:46:57 +0300

    min ve max değer bulma güncellendi

--- Donguler/minVeMaxDegerBulma.java
+++ Donguler/minVeMaxDegerBulma.java
@@ -7,20 +7,18 @@ public class minVeMaxDegerBulma {
         Scanner input = new Scanner(System.in);
         int toplam=0;
         int n=0;
-        int max, min;
+        int max=0, min=0;
 
         System.out.print("Toplam Kaç Sayı Girilecek : ");
-        toplam = input.nextInt();
+        toplam = input.nextInt();        
 
-        System.out.print("1. sayıyı giriniz : ");
+        for (int i=0;i<toplam;i++){
+            System.out.print((i+1) + ". sayıyı giriniz : ");
             n = input.nextInt();
-            max=n;
-            min=n;
-
-        for (int i=0;i<(toplam-1);i++){
-            System.out.print((i+2) + ". sayıyı giriniz : ");
-            n = input.nextInt();
-
+            if(i==0){
+                max=n;
+                min=n;
+            }           
             if(n>max){
                 max=n;
             }
