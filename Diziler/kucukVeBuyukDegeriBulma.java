7a1efc772cccd6003ab710ab0c1691d099e6342e
alisimsek <ali.simsek035@gmail.com>
2022-03-27 18:58:31 +0300

    en yakın küçük ve büyük sayıyı bulma programı eklendi. www.patika.dev

---
+++ Diziler/kucukVeBuyukDegeriBulma.java
@@ -0,0 +1,35 @@
+package Diziler;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class kucukVeBuyukDegeriBulma {
+    public static void main(String[] args){
+        int [] list = {15,12,788,1,-1,-778,2,0};
+        int a;
+        int x=0;
+        int y=0;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Dizi : " + Arrays.toString(list));
+        System.out.print("Girilen sayı : ");
+        a = input.nextInt();
+        
+        Arrays.sort(list);
+        
+        for (int i =list.length-1; i>=0;i--){
+            if(a>list[i]){
+                x = list[i];
+                break;
+            }
+        }
+        for (int j =0; j<list.length-1;j++){
+            if(a<list[j]){
+                y = list[j];
+                break;
+            }
+        }
+
+        System.out.println("Girilen sayıdan küçük en yakın sayı : " + x);
+        System.out.println("Girilen sayıdan büyük en yakın sayı : " + y);
+    }
+} //www.patika.dev
